9def019d2ba941a650f172a7b4ecf5d1cefc269f
pavelvasilevich24 <pavelvasilevich24@gmail.com>
2025-01-06 16:46:35 +0300

    feat: queue

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const node = new ListNode(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    }
+    else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
+  
+  getUnderlyingList() {
+    return this.head;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const current = this.head;
+    this.head = this.head.next;
+    return current.value;
   }
 }
 
